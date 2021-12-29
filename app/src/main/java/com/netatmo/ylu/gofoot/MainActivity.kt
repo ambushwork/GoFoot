4b82a7988e8bd4bd0be3a5260702ffb7f5dfc0f4
ambushwork <ambushwork@live.cn>
2021-12-29 21:21:43 +0100

    Setup project

---
+++ app/src/main/java/com/netatmo/ylu/gofoot/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.netatmo.ylu.gofoot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
